a678e21c96f363c1b959228c063fef8d096dfd25
Tim Haines <thaines@astro.wisc.edu>
2016-09-03 17:54:24 -0500

    Update rsqrt; add DP version

--- cksimd_sse.h
+++ cksimd_sse.h
@@ -22,10 +22,36 @@ namespace ck_simd {
 		return _mm_sqrt_pd(x);
 	}
 	inline __m128 rsqrt(__m128 x, sse_float_tag) {
-		return _mm_rsqrt_ps(x);
+		/**
+		 * 	Do one Newton-Raphson iteration to bring the precision to ~23
+		 * 	bits (~2e-7). This works equally well on Bulldozer, Piledriver,
+		 *	Sandybridge, and Skylake.
+		 */
+		const __m128 three = _mm_set1_ps(3.0f), half = _mm_set1_ps(0.5f);
+		const __m128 rsrt = _mm_rsqrt_ps(x);
+		const __m128 muls = _mm_mul_ps(_mm_mul_ps(x, rsrt), rsrt);
+		return _mm_mul_ps(_mm_mul_ps(half, rsrt), _mm_sub_ps(three, muls));
 	}
-	inline __m128d rsqrt(__m128d x, sse_double_tag) {
-		return _mm_div_pd(_mm_set1_pd(1.0), _mm_sqrt_pd(x));
+	inline __m128d rsqrt(__m128d a, sse_double_tag) {
+		/**
+		* 	This routine is adapted from
+		* 	https://github.com/stgatilov/recip_rsqrt_benchmark
+		*
+		* 	It uses a 5th-order polynomial to bring the error to 51.5 bits (~3e-16).
+		*
+		* 	The speedup compared to using sqrt+div varies by CPU architecture, but
+		* 	is between 10% (Skylake) and 3x (Sandybridge) faster.
+		*/
+		__m128d one = _mm_set1_pd(1.0), 	c1 = _mm_set1_pd(1.0/2.0),
+				c2 = _mm_set1_pd(3.0/8.0), 	c3 = _mm_set1_pd(15.0/48.0),
+				c4 = _mm_set1_pd(105.0/384.0);
+		__m128d x = _mm_cvtps_pd(_mm_rsqrt_ps(_mm_cvtpd_ps(a)));
+		__m128d r = _mm_sub_pd(one, _mm_mul_pd(_mm_mul_pd(a, x), x));
+		__m128d r2 = _mm_mul_pd(r, r);
+		__m128d t1 = _mm_add_pd(_mm_mul_pd(c2, r), c1);
+		__m128d t3 = _mm_add_pd(_mm_mul_pd(c4, r), c3);
+		__m128d poly = _mm_add_pd(_mm_mul_pd(r2, t3), t1);
+		return _mm_add_pd(_mm_mul_pd(_mm_mul_pd(x, r), poly), x);
 	}
 	inline __m128 zero(sse_float_tag) {
 		return _mm_setzero_ps();
