ed5fc6f21561a6547d5a225fbba2802e905fd3be
Tim Haines <thaines@astro.wisc.edu>
2016-09-03 05:12:26 -0500

    Several bug fixes

--- cksimd_test_legacy.cpp
+++ cksimd_test_legacy.cpp
@@ -33,7 +33,7 @@ bool to_bool(T *x, size_t n) {
 }
 bool to_bool(SSEFloat f) {
 	float *x = reinterpret_cast<float*>(&(f.val));
-	return to_bool(x, sizeof(SSEFloat));
+	return to_bool(x, 4);
 }
 bool to_bool(SSEDouble f) {
 	double *x = reinterpret_cast<double*>(&(f.val));
@@ -59,18 +59,23 @@ template<>
 void combine(SSEDouble &mask, bool v) {
 	mask = mask & SSEDouble(double(v));
 }
+std::ostream& operator<<(std::ostream &o, SSEFloat f) {
+	float *x = reinterpret_cast<float*>(&(f.val));
+	return o << '{' << x[0] << ',' << x[1] << ','
+			        << x[2] << ',' << x[3] << '}';
+}
 template <typename T, typename U>
 void test(T x, U y, char const* name, answer<T,U> const& ans) {
-	asm volatile("sqrt_begin%=:" :);
+	asm volatile("simd_test_begin%=:" :);
 	auto srty = U(sqrt(y));
 	auto rsrt = x / sqrt(y);
-	asm volatile("sqrt_end%=:" :);
 	auto a = ((x + y) == ans.sum ) &
 			 ((x - y) == ans.diff) &
 			 ((x * y) == ans.prod) &
 			 ((x / y) == ans.quot);
 	combine(a, srty == ans.srty);
 	combine(a, rsrt == ans.rsrt);
+	asm volatile("simd_test_end%=:" :);
 
 	if(!to_bool(a)) {
 		std::cerr << '\n' << name << " FAILED\n";
@@ -88,13 +93,18 @@ int main() {
 	float srty = sqrt(p2);
 	float rsrt = p1 / sqrt(p2);
 
+	// No AVX support for SP
+#ifdef CMK_USE_SSE2
 	test(SSEFloat{p1}, SSEFloat{p2}, "SSEFloat+SSEFloat", answer<SSEFloat, SSEFloat>{sum,diff,prod,quot,srty,rsrt});
 	test(SSEFloat{p1}, p2, "SSEFloat+float", answer<SSEFloat, float>{sum,diff,prod,quot,srty,rsrt});
 	test(p1, SSEFloat{p2}, "float+SSEFloat", answer<float, SSEFloat>{sum,diff,prod,quot,srty,rsrt});
 	test(p1, p2, "float+float", answer<float, float>{sum,diff,prod,quot,srty,rsrt});
+#endif
+
 	test(SSEDouble{p1}, SSEDouble{p2}, "SSEDouble+SSEDouble", answer<SSEDouble, SSEDouble>{sum,diff,prod,quot,srty,rsrt});
 	test(SSEDouble{p1}, double{p2}, "SSEDouble+double", answer<SSEDouble, double>{sum,diff,prod,quot,srty,rsrt});
 	test(double{p1}, SSEDouble{p2}, "double+SSEDouble", answer<double, SSEDouble>{sum,diff,prod,quot,srty,rsrt});
 	test(double{p1}, double{p2}, "double+double", answer<double, double>{sum,diff,prod,quot,srty,rsrt});
-	std::cout << "PASSED\n";
+	std::cout << "DONE\n";
 }
+
