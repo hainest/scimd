40f2cfede0832a79576fa809a835cbc77ba0bf03
Tim Haines <thaines@astro.wisc.edu>
2016-09-03 17:59:06 -0500

    Make all SIMD helper functions static to reduce linkage overhead

--- cksimd_scalar.h
+++ cksimd_scalar.h
@@ -35,150 +35,150 @@ namespace ck_simd {
 	 * 	Tag dispatch is used here because the gcc ABI before gcc-4.9
 	 * 	does not properly mangle the SIMD types.
 	 */
-	inline float sqrt(float x, scalar_float_tag) {
+	static inline float sqrt(float x, scalar_float_tag) {
 		return std::sqrt(x);
 	}
-	inline double sqrt(double x, scalar_double_tag) {
+	static inline double sqrt(double x, scalar_double_tag) {
 		return std::sqrt(x);
 	}
-	inline float rsqrt(float x, scalar_float_tag) {
+	static inline float rsqrt(float x, scalar_float_tag) {
 		return 1.0f / std::sqrt(x);
 	}
-	inline double rsqrt(double x, scalar_double_tag) {
+	static inline double rsqrt(double x, scalar_double_tag) {
 		return 1.0 / std::sqrt(x);
 	}
-	inline float zero(scalar_float_tag) {
+	static inline float zero(scalar_float_tag) {
 		return 0.0f;
 	}
-	inline double zero(scalar_double_tag) {
+	static inline double zero(scalar_double_tag) {
 		return 0.0;
 	}
-	inline float set1(float x, scalar_float_tag) {
+	static inline float set1(float x, scalar_float_tag) {
 		return x;
 	}
-	inline double set1(double x, scalar_double_tag) {
+	static inline double set1(double x, scalar_double_tag) {
 		return x;
 	}
-	inline float neg(float x, scalar_float_tag) {
+	static inline float neg(float x, scalar_float_tag) {
 		return -x;
 	}
-	inline double neg(double x, scalar_double_tag) {
+	static inline double neg(double x, scalar_double_tag) {
 		return -x;
 	}
-	inline float add(float x, float y, scalar_float_tag) {
+	static inline float add(float x, float y, scalar_float_tag) {
 		return x + y;
 	}
-	inline double add(double x, double y, scalar_double_tag) {
+	static inline double add(double x, double y, scalar_double_tag) {
 		return x + y;
 	}
-	inline float sub(float x, float y, scalar_float_tag) {
+	static inline float sub(float x, float y, scalar_float_tag) {
 		return x - y;
 	}
-	inline double sub(double x, double y, scalar_double_tag) {
+	static inline double sub(double x, double y, scalar_double_tag) {
 		return x - y;
 	}
-	inline float mul(float x, float y, scalar_float_tag) {
+	static inline float mul(float x, float y, scalar_float_tag) {
 		return x * y;
 	}
-	inline double mul(double x, double y, scalar_double_tag) {
+	static inline double mul(double x, double y, scalar_double_tag) {
 		return x * y;
 	}
-	inline float div(float x, float y, scalar_float_tag) {
+	static inline float div(float x, float y, scalar_float_tag) {
 		return x / y;
 	}
-	inline double div(double x, double y, scalar_double_tag) {
+	static inline double div(double x, double y, scalar_double_tag) {
 		return x / y;
 	}
-	inline float mask_and(float x, float y, scalar_float_tag) {
+	static inline float mask_and(float x, float y, scalar_float_tag) {
 		return maskf_t(maskf_t(x).i & maskf_t(y).i).f;
 	}
-	inline double mask_and(double x, double y, scalar_double_tag) {
+	static inline double mask_and(double x, double y, scalar_double_tag) {
 		return mask_t(mask_t(x).i & mask_t(y).i).d;
 	}
-	inline float mask_or(float x, float y, scalar_float_tag) {
+	static inline float mask_or(float x, float y, scalar_float_tag) {
 		return maskf_t(maskf_t(x).i | maskf_t(y).i).f;
 	}
-	inline double mask_or(double x, double y, scalar_double_tag) {
+	static inline double mask_or(double x, double y, scalar_double_tag) {
 		return mask_t(mask_t(x).i | mask_t(y).i).d;
 	}
-	inline float mask_xor(float x, float y, scalar_float_tag) {
+	static inline float mask_xor(float x, float y, scalar_float_tag) {
 		return maskf_t(maskf_t(x).i ^ maskf_t(y).i).f;
 	}
-	inline double mask_xor(double x, double y, scalar_double_tag) {
+	static inline double mask_xor(double x, double y, scalar_double_tag) {
 		return mask_t(mask_t(x).i ^ mask_t(y).i).d;
 	}
-	inline float mask_andnot(float x, float y, scalar_float_tag) {
+	static inline float mask_andnot(float x, float y, scalar_float_tag) {
 		return maskf_t(~(maskf_t(x).i) & maskf_t(y).i).f;
 	}
-	inline double mask_andnot(double x, double y, scalar_double_tag) {
+	static inline double mask_andnot(double x, double y, scalar_double_tag) {
 		return mask_t(~(mask_t(x).i) & mask_t(y).i).d;
 	}
-	inline float less(float x, float y, scalar_float_tag) {
+	static inline float less(float x, float y, scalar_float_tag) {
 		return (x < y) ? maskf_t(true_mask_float).f : 0;
 	}
-	inline double less(double x, double y, scalar_double_tag) {
+	static inline double less(double x, double y, scalar_double_tag) {
 		return (x < y) ? mask_t(true_mask_double).d : 0;
 	}
-	inline float greater(float x, float y, scalar_float_tag) {
+	static inline float greater(float x, float y, scalar_float_tag) {
 		return (x > y) ? maskf_t(true_mask_float).f : 0;
 	}
-	inline double greater(double x, double y, scalar_double_tag) {
+	static inline double greater(double x, double y, scalar_double_tag) {
 		return (x > y) ? mask_t(true_mask_double).d : 0;
 	}
-	inline float less_eq(float x, float y, scalar_float_tag) {
+	static inline float less_eq(float x, float y, scalar_float_tag) {
 		return (x <= y) ? maskf_t(true_mask_float).f : 0;
 	}
-	inline double less_eq(double x, double y, scalar_double_tag) {
+	static inline double less_eq(double x, double y, scalar_double_tag) {
 		return (x <= y) ? mask_t(true_mask_double).d : 0;
 	}
-	inline float greater_eq(float x, float y, scalar_float_tag) {
+	static inline float greater_eq(float x, float y, scalar_float_tag) {
 		return (x >= y) ? maskf_t(true_mask_float).f : 0;
 	}
-	inline double greater_eq(double x, double y, scalar_double_tag) {
+	static inline double greater_eq(double x, double y, scalar_double_tag) {
 		return (x >= y) ? mask_t(true_mask_double).d : 0;
 	}
-	inline float equals(float x, float y, scalar_float_tag) {
+	static inline float equals(float x, float y, scalar_float_tag) {
 		return (x == y) ? maskf_t(true_mask_float).f : 0;
 	}
-	inline double equals(double x, double y, scalar_double_tag) {
+	static inline double equals(double x, double y, scalar_double_tag) {
 		return (x == y) ? mask_t(true_mask_double).d : 0;
 	}
-	inline int movemask(float x, scalar_float_tag) {
+	static inline int movemask(float x, scalar_float_tag) {
 		/**
 		 *  Emulate the movmskps instruction.
 		 * 	Extract the sign bit from x and format it into a 4-bit mask
 		 */
 		return (x < 0.0f) ? 0xf : 0;
 	}
-	inline int movemask(double x, scalar_double_tag) {
+	static inline int movemask(double x, scalar_double_tag) {
 		/**
 		 *  Emulate the movmskpd instruction.
 		 * 	Extract the sign bit from x and format it into a 2-bit mask
 		 */
 		return (x < 0.0) ? 0x3 : 0;
 	}
-	inline void storeu(float *p, float x, scalar_float_tag) {
+	static inline void storeu(float *p, float x, scalar_float_tag) {
 		*p = x;
 	}
-	inline void storeu(double *p, double x, scalar_double_tag) {
+	static inline void storeu(double *p, double x, scalar_double_tag) {
 		*p = x;
 	}
-	inline float max(float x, float y, scalar_float_tag) {
+	static inline float max(float x, float y, scalar_float_tag) {
 		return std::max(x,y);
 	}
-	inline double max(double x, double y, scalar_double_tag) {
+	static inline double max(double x, double y, scalar_double_tag) {
 		return std::max(x,y);
 	}
-	inline float min(float x, float y, scalar_float_tag) {
+	static inline float min(float x, float y, scalar_float_tag) {
 		return std::min(x,y);
 	}
-	inline double min(double x, double y, scalar_double_tag) {
+	static inline double min(double x, double y, scalar_double_tag) {
 		return std::min(x,y);
 	}
-	inline float loadu(float const* p, scalar_float_tag) {
+	static inline float loadu(float const* p, scalar_float_tag) {
 		return *p;
 	}
-	inline double loadu(double const* p, scalar_double_tag) {
+	static inline double loadu(double const* p, scalar_double_tag) {
 		return *p;
 	}
 };
--- cksimd_sse.h
+++ cksimd_sse.h
@@ -15,13 +15,13 @@ namespace ck_simd {
 	 * 	Tag dispatch is used here because the gcc ABI before gcc-4.9
 	 * 	does not properly mangle the SIMD types.
 	 */
-	inline __m128 sqrt(__m128 x, sse_float_tag) {
+	static inline __m128 sqrt(__m128 x, sse_float_tag) {
 		return _mm_sqrt_ps(x);
 	}
-	inline __m128d sqrt(__m128d x, sse_double_tag) {
+	static inline __m128d sqrt(__m128d x, sse_double_tag) {
 		return _mm_sqrt_pd(x);
 	}
-	inline __m128 rsqrt(__m128 x, sse_float_tag) {
+	static inline __m128 rsqrt(__m128 x, sse_float_tag) {
 		/**
 		 * 	Do one Newton-Raphson iteration to bring the precision to ~23
 		 * 	bits (~2e-7). This works equally well on Bulldozer, Piledriver,
@@ -32,7 +32,7 @@ namespace ck_simd {
 		const __m128 muls = _mm_mul_ps(_mm_mul_ps(x, rsrt), rsrt);
 		return _mm_mul_ps(_mm_mul_ps(half, rsrt), _mm_sub_ps(three, muls));
 	}
-	inline __m128d rsqrt(__m128d a, sse_double_tag) {
+	static inline __m128d rsqrt(__m128d a, sse_double_tag) {
 		/**
 		* 	This routine is adapted from
 		* 	https://github.com/stgatilov/recip_rsqrt_benchmark
@@ -53,130 +53,130 @@ namespace ck_simd {
 		__m128d poly = _mm_add_pd(_mm_mul_pd(r2, t3), t1);
 		return _mm_add_pd(_mm_mul_pd(_mm_mul_pd(x, r), poly), x);
 	}
-	inline __m128 zero(sse_float_tag) {
+	static inline __m128 zero(sse_float_tag) {
 		return _mm_setzero_ps();
 	}
-	inline __m128d zero(sse_double_tag) {
+	static inline __m128d zero(sse_double_tag) {
 		return _mm_setzero_pd();
 	}
-	inline __m128 set1(float x, sse_float_tag) {
+	static inline __m128 set1(float x, sse_float_tag) {
 		return _mm_set1_ps(x);
 	}
-	inline __m128d set1(double x, sse_double_tag) {
+	static inline __m128d set1(double x, sse_double_tag) {
 		return _mm_set1_pd(x);
 	}
-	inline __m128 neg(__m128 x, sse_float_tag) {
+	static inline __m128 neg(__m128 x, sse_float_tag) {
 		return _mm_sub_ps(_mm_setzero_ps(), x);
 	}
-	inline __m128d neg(__m128d x, sse_double_tag) {
+	static inline __m128d neg(__m128d x, sse_double_tag) {
 		return _mm_sub_pd(_mm_setzero_pd(), x);
 	}
-	inline __m128 add(__m128 x, __m128 y, sse_float_tag) {
+	static inline __m128 add(__m128 x, __m128 y, sse_float_tag) {
 		return _mm_add_ps(x, y);
 	}
-	inline __m128d add(__m128d x, __m128d y, sse_double_tag) {
+	static inline __m128d add(__m128d x, __m128d y, sse_double_tag) {
 		return _mm_add_pd(x, y);
 	}
-	inline __m128 sub(__m128 x, __m128 y, sse_float_tag) {
+	static inline __m128 sub(__m128 x, __m128 y, sse_float_tag) {
 		return _mm_sub_ps(x, y);
 	}
-	inline __m128d sub(__m128d x, __m128d y, sse_double_tag) {
+	static inline __m128d sub(__m128d x, __m128d y, sse_double_tag) {
 		return _mm_sub_pd(x, y);
 	}
-	inline __m128 mul(__m128 x, __m128 y, sse_float_tag) {
+	static inline __m128 mul(__m128 x, __m128 y, sse_float_tag) {
 		return _mm_mul_ps(x, y);
 	}
-	inline __m128d mul(__m128d x, __m128d y, sse_double_tag) {
+	static inline __m128d mul(__m128d x, __m128d y, sse_double_tag) {
 		return _mm_mul_pd(x, y);
 	}
-	inline __m128 div(__m128 x, __m128 y, sse_float_tag) {
+	static inline __m128 div(__m128 x, __m128 y, sse_float_tag) {
 		return _mm_div_ps(x, y);
 	}
-	inline __m128d div(__m128d x, __m128d y, sse_double_tag) {
+	static inline __m128d div(__m128d x, __m128d y, sse_double_tag) {
 		return _mm_div_pd(x, y);
 	}
-	inline __m128 mask_and(__m128 x, __m128 y, sse_float_tag) {
+	static inline __m128 mask_and(__m128 x, __m128 y, sse_float_tag) {
 		return _mm_and_ps(x, y);
 	}
-	inline __m128d mask_and(__m128d x, __m128d y, sse_double_tag) {
+	static inline __m128d mask_and(__m128d x, __m128d y, sse_double_tag) {
 		return _mm_and_pd(x, y);
 	}
-	inline __m128 mask_or(__m128 x, __m128 y, sse_float_tag) {
+	static inline __m128 mask_or(__m128 x, __m128 y, sse_float_tag) {
 		return _mm_or_ps(x, y);
 	}
-	inline __m128d mask_or(__m128d x, __m128d y, sse_double_tag) {
+	static inline __m128d mask_or(__m128d x, __m128d y, sse_double_tag) {
 		return _mm_or_pd(x, y);
 	}
-	inline __m128 mask_xor(__m128 x, __m128 y, sse_float_tag) {
+	static inline __m128 mask_xor(__m128 x, __m128 y, sse_float_tag) {
 		return _mm_xor_ps(x, y);
 	}
-	inline __m128d mask_xor(__m128d x, __m128d y, sse_double_tag) {
+	static inline __m128d mask_xor(__m128d x, __m128d y, sse_double_tag) {
 		return _mm_xor_pd(x, y);
 	}
-	inline __m128 mask_andnot(__m128 x, __m128 y, sse_float_tag) {
+	static inline __m128 mask_andnot(__m128 x, __m128 y, sse_float_tag) {
 		return _mm_andnot_ps(x, y);
 	}
-	inline __m128d mask_andnot(__m128d x, __m128d y, sse_double_tag) {
+	static inline __m128d mask_andnot(__m128d x, __m128d y, sse_double_tag) {
 		return _mm_andnot_pd(x, y);
 	}
-	inline __m128 less(__m128 x, __m128 y, sse_float_tag) {
+	static inline __m128 less(__m128 x, __m128 y, sse_float_tag) {
 		return _mm_cmplt_ps(x, y);
 	}
-	inline __m128d less(__m128d x, __m128d y, sse_double_tag) {
+	static inline __m128d less(__m128d x, __m128d y, sse_double_tag) {
 		return _mm_cmplt_pd(x, y);
 	}
-	inline __m128 greater(__m128 x, __m128 y, sse_float_tag) {
+	static inline __m128 greater(__m128 x, __m128 y, sse_float_tag) {
 		return _mm_cmpgt_ps(x, y);
 	}
-	inline __m128d greater(__m128d x, __m128d y, sse_double_tag) {
+	static inline __m128d greater(__m128d x, __m128d y, sse_double_tag) {
 		return _mm_cmpgt_pd(x, y);
 	}
-	inline __m128 less_eq(__m128 x, __m128 y, sse_float_tag) {
+	static inline __m128 less_eq(__m128 x, __m128 y, sse_float_tag) {
 		return _mm_cmple_ps(x, y);
 	}
-	inline __m128d less_eq(__m128d x, __m128d y, sse_double_tag) {
+	static inline __m128d less_eq(__m128d x, __m128d y, sse_double_tag) {
 		return _mm_cmple_pd(x, y);
 	}
-	inline __m128 greater_eq(__m128 x, __m128 y, sse_float_tag) {
+	static inline __m128 greater_eq(__m128 x, __m128 y, sse_float_tag) {
 		return _mm_cmpge_ps(x, y);
 	}
-	inline __m128d greater_eq(__m128d x, __m128d y, sse_double_tag) {
+	static inline __m128d greater_eq(__m128d x, __m128d y, sse_double_tag) {
 		return _mm_cmpge_pd(x, y);
 	}
-	inline __m128 equals(__m128 x, __m128 y, sse_float_tag) {
+	static inline __m128 equals(__m128 x, __m128 y, sse_float_tag) {
 		return _mm_cmpeq_ps(x, y);
 	}
-	inline __m128d equals(__m128d x, __m128d y, sse_double_tag) {
+	static inline __m128d equals(__m128d x, __m128d y, sse_double_tag) {
 		return _mm_cmpeq_pd(x, y);
 	}
-	inline int movemask(__m128 x, sse_float_tag) {
+	static inline int movemask(__m128 x, sse_float_tag) {
 		return _mm_movemask_ps(x);
 	}
-	inline int movemask(__m128d x, sse_double_tag) {
+	static inline int movemask(__m128d x, sse_double_tag) {
 		return _mm_movemask_pd(x);
 	}
-	inline void storeu(float *p, __m128 x, sse_float_tag) {
+	static inline void storeu(float *p, __m128 x, sse_float_tag) {
 		_mm_storeu_ps(p, x);
 	}
-	inline void storeu(double *p, __m128d x, sse_double_tag) {
+	static inline void storeu(double *p, __m128d x, sse_double_tag) {
 		_mm_storeu_pd(p, x);
 	}
-	inline __m128 max(__m128 x, __m128 y, sse_float_tag) {
+	static inline __m128 max(__m128 x, __m128 y, sse_float_tag) {
 		return _mm_max_ps(x, y);
 	}
-	inline __m128d max(__m128d x, __m128d y, sse_double_tag) {
+	static inline __m128d max(__m128d x, __m128d y, sse_double_tag) {
 		return _mm_max_pd(x, y);
 	}
-	inline __m128 min(__m128 x, __m128 y, sse_float_tag) {
+	static inline __m128 min(__m128 x, __m128 y, sse_float_tag) {
 		return _mm_min_ps(x, y);
 	}
-	inline __m128d min(__m128d x, __m128d y, sse_double_tag) {
+	static inline __m128d min(__m128d x, __m128d y, sse_double_tag) {
 		return _mm_min_pd(x, y);
 	}
-	inline __m128 loadu(float const* p, sse_float_tag) {
+	static inline __m128 loadu(float const* p, sse_float_tag) {
 		return _mm_loadu_ps(p);
 	}
-	inline __m128d loadu(double const* p, sse_double_tag) {
+	static inline __m128d loadu(double const* p, sse_double_tag) {
 		return _mm_loadu_pd(p);
 	}
 };
