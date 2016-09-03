ef1079b76d45ed05c2d6d5271d3ee8b0649640f8
Tim Haines <thaines@astro.wisc.edu>
2016-09-03 06:55:01 -0500

    Several bug fixes; use tolerances for checking equality

--- cksimd_test.cpp
+++ cksimd_test.cpp
@@ -2,16 +2,19 @@
 #include <iostream>
 #include <iomanip>
 #include <cmath>
+#include <cstdlib>
 
+union mask_t  {
+	double d;
+	uint64_t i;
+	mask_t(double d) : d(d) {}
+	mask_t(uint64_t i) : i(i) {}
+};
 union maskf_t {
 	float f;
 	uint32_t i;
 	maskf_t(float f) : f(f) {}
-};
-union mask_t {
-	double d;
-	uint64_t i;
-	mask_t(double d) : d(d) {}
+	maskf_t(uint32_t i) : i(i) {}
 };
 template <typename T>
 struct mask {};
@@ -19,8 +22,8 @@ template <> struct mask<float>  { typedef maskf_t type; };
 template <> struct mask<double> { typedef mask_t type; };
 template <typename T>
 bool to_bool(T *x, size_t n) {
-	uint32_t r{0};
 	typedef typename mask<T>::type mask_type;
+	decltype(mask_type::i) r{0};
 	for(size_t i=0; i<n; ++i) {
 		r |= mask_type(x[i]).i;
 	}
@@ -30,11 +33,15 @@ template <typename T>
 bool to_bool(T f) {
 	typedef typename T::value_type value_type;
 	value_type *x = reinterpret_cast<value_type*>(&(f.val));
-	return to_bool(x, T::size);
+	return to_bool(x, T::nelem);
 }
 template<>
-bool to_bool(int f) {
-	return f;
+bool to_bool(bool b) {
+	return b;
+}
+template<>
+bool to_bool(int i) {
+	return i != 0;
 }
 template <typename T, typename U>
 struct answer {
@@ -46,12 +53,12 @@ void combine(T &mask, U v) {
 	mask &= v;
 }
 template<>
-void combine(SSEFloat &f, bool v) {
-	f &= SSEFloat(float(v));
+void combine(cksimd<float> &f, bool v) {
+	f &= cksimd<float>(float(v));
 }
 template<>
-void combine(SSEDouble &f, bool v) {
-	f &= SSEDouble(double(v));
+void combine(cksimd<double> &f, bool v) {
+	f &= cksimd<double>(double(v));
 }
 template <typename T>
 std::ostream& operator<<(std::ostream &o, cksimd<T> f) {
@@ -70,42 +77,59 @@ std::ostream& operator<<(std::ostream &o, cksimd<T> f) {
 	}
 	return o << '}';
 }
-template <typename T, typename U>
-void test(T x, U y, char const* name, answer<T,U> const& ans) {
-	asm volatile("sqrt_begin%=:" :);
+template <typename T, typename U, typename V>
+void test(T x, U y, char const* name, answer<T,U> const& ans, V tol) {
+	asm volatile("simd_test_begin%=:" :);
 	auto srty = U(sqrt(y));
 	auto rsrt = x / sqrt(y);
-	asm volatile("sqrt_end%=:" :);
-	auto a = ((x + y) == ans.sum ) &
-			 ((x - y) == ans.diff) &
-			 ((x * y) == ans.prod) &
-			 ((x / y) == ans.quot);
-	combine(a, srty == ans.srty);
-	combine(a, rsrt == ans.rsrt);
+	auto a = (((x + y) - ans.sum)  <= tol) &
+			 (((x - y) - ans.diff) <= tol) &
+			 (((x * y) - ans.prod) <= tol) &
+			 (((x / y) - ans.quot) <= tol);
+	combine(a, (srty - ans.srty) <= tol);
+	combine(a, (rsrt - ans.rsrt) <= tol);
+	asm volatile("simd_test_end%=:" :);
 
 	if(!to_bool(a)) {
-		std::cerr << '\n' << name << " FAILED\n";
+		std::cerr << name << " FAILED\n";
+		std::exit(-1);
 	}
 }
 
 int main() {
-	float p1 = 4.0f;
-	float p2 = 16.0f;
+	{
+		float x = 3.0f;
+		float y = 17.0f;
+		float sum = x + y;
+		float diff = x - y;
+		float prod = x * y;
+		float quot = x / y;
+		float srty = sqrt(y);
+		float rsrt = x / sqrt(y);
+		const float tol = 2e-07;
 
-	float sum = p1 + p2;
-	float diff = p1 - p2;
-	float prod = p1 * p2;
-	float quot = p1 / p2;
-	float srty = sqrt(p2);
-	float rsrt = p1 / sqrt(p2);
+		test(cksimd<float>{x}, cksimd<float>{y}, "cksimd<float>+cksimd<float>", answer<cksimd<float>, cksimd<float>>{sum,diff,prod,quot,srty,rsrt}, tol);
+		test(cksimd<float>{x}, y, "cksimd<float>+float", answer<cksimd<float>, float>{sum,diff,prod,quot,srty,rsrt}, tol);
+		test(x, cksimd<float>{y}, "float+cksimd<float>", answer<float, cksimd<float>>{sum,diff,prod,quot,srty,rsrt}, tol);
+		test(x, y, "float+float", answer<float, float>{sum,diff,prod,quot,srty,rsrt}, tol);
+	}
 
-	test(SSEFloat{p1}, SSEFloat{p2}, "SSEFloat+SSEFloat", answer<SSEFloat, SSEFloat>{sum,diff,prod,quot,srty,rsrt});
-	test(SSEFloat{p1}, p2, "SSEFloat+float", answer<SSEFloat, float>{sum,diff,prod,quot,srty,rsrt});
-	test(p1, SSEFloat{p2}, "float+SSEFloat", answer<float, SSEFloat>{sum,diff,prod,quot,srty,rsrt});
-	test(p1, p2, "float+float", answer<float, float>{sum,diff,prod,quot,srty,rsrt});
-	test(SSEDouble{p1}, SSEDouble{p2}, "SSEDouble+SSEDouble", answer<SSEDouble, SSEDouble>{sum,diff,prod,quot,srty,rsrt});
-	test(SSEDouble{p1}, double{p2}, "SSEDouble+double", answer<SSEDouble, double>{sum,diff,prod,quot,srty,rsrt});
-	test(double{p1}, SSEDouble{p2}, "double+SSEDouble", answer<double, SSEDouble>{sum,diff,prod,quot,srty,rsrt});
-	test(double{p1}, double{p2}, "double+double", answer<double, double>{sum,diff,prod,quot,srty,rsrt});
-	std::cout << "PASSED\n";
+	{
+		double x = 3.0;
+		double y = 17.0;
+		double sum = x + y;
+		double diff = x - y;
+		double prod = x * y;
+		double quot = x / y;
+		double srty = sqrt(y);
+		double rsrt = x / sqrt(y);
+		const double tol = 2e-16;
+
+		test(cksimd<double>{x}, cksimd<double>{y}, "cksimd<double>+cksimd<double>", answer<cksimd<double>, cksimd<double>>{sum,diff,prod,quot,srty,rsrt}, tol);
+		test(cksimd<double>{x}, y, "cksimd<double>+double", answer<cksimd<double>, double>{sum,diff,prod,quot,srty,rsrt}, tol);
+		test(x, cksimd<double>{y}, "double+cksimd<double>", answer<double, cksimd<double>>{sum,diff,prod,quot,srty,rsrt}, tol);
+		test(x, y, "double+double", answer<double, double>{sum,diff,prod,quot,srty,rsrt}, tol);
+	}
+
+	std::cout << "DONE\n";
 }
