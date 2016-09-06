5ece56448fa23e9b3eef34e90cef24426c71f8a4
tim <tim@Mandelbrot>
2016-09-06 05:59:42 -0500

    Add alignment policy

--- cksimd_traits.h
+++ cksimd_traits.h
@@ -7,15 +7,22 @@ namespace ck_simd {
 	template <typename T>
 	struct simd_category {};
 
-	template <typename T>
-	struct simd_type {};
-
+	// Category tags
 	struct sse_float_tag {};
 	struct sse_double_tag {};
 	struct avx_float_tag {};
 	struct avx_double_tag {};
 	struct scalar_float_tag {};
 	struct scalar_double_tag {};
+
+	// Alignment tags
+	struct aligned_load_tag {};
+	struct unaligned_load_tag {};
+	struct aligned_store_tag {};
+	struct unaligned_store_tag {};
+
+	template <typename T>
+	struct simd_type {};
 
 	template <typename T>
 	struct is_scalar {
