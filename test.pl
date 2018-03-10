4ac9674a301ca6bcb22709784e8258a771293195
Tim Haines <thaines.astro@gmail.com>
2018-03-10 12:37:50 -0600

    Add clang-4.0 to tested compilers

--- test.pl
+++ test.pl
@@ -7,7 +7,7 @@ sub execute($) {
 	die "\n\nError executing \n\t'$cmd'\n\n" if ( ( $? >> 8 ) != 0 || $? == -1 || ( $? & 127 ) != 0 );
 }
 
-my @compilers = (@ARGV) ? @ARGV : ('g++-4.8', 'g++-4.9', 'g++-5', 'g++-6', 'clang++-3.7', 'clang++-3.8', 'clang++-3.9');
+my @compilers = (@ARGV) ? @ARGV : ('g++-4.8', 'g++-4.9', 'g++-5', 'g++-6', 'clang++-3.7', 'clang++-3.8', 'clang++-3.9', 'clang++-4.0');
 my @architectures = ('sse', 'avx', 'fma', 'scalar');
 
 for my $c (@compilers) {
